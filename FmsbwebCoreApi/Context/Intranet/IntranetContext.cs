797f3b19b3a917c1eee032949cb7b17d658a7886
aevi1103 <aevirontos@gmail.com>
2020-09-02 13:55:13 -0400

    manning tables

--- FmsbwebCoreApi/Context/Intranet/IntranetContext.cs
+++ FmsbwebCoreApi/Context/Intranet/IntranetContext.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using FmsbwebCoreApi.Entity.Intranet;
@@ -22,14 +23,49 @@ namespace FmsbwebCoreApi.Context.Intranet
         public virtual DbSet<FmsbMasterProdPartsCopyDashboardProgram> FmsbMasterProdPartsCopyDashboardProgram { get; set; }
         public virtual DbSet<FmsbMasterProdPartsCopyView> FmsbMasterProdPartsCopyView { get; set; }
         public virtual DbSet<FmsbMasterProdPartsFoundrySapScrap> FmsbMasterProdPartsFoundrySapScrap { get; set; }
+        public virtual DbSet<Operator> Operators { get; set; }
+        public virtual DbSet<OperatorJob> OperatorJobs { get; set; }
+        public virtual DbSet<EolManning> EolManning { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-//            if (!optionsBuilder.IsConfigured)
-//            {
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-//                optionsBuilder.UseSqlServer("Data Source=SBNDINMS002;Initial Catalog=FMO_Intranet;Integrated Security=False;User ID=rontoa20;Password=aebbie17;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-//            }
+        }
+
+        private static void SeedData(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<OperatorJob>().HasData(new OperatorJob { OperatorJobId = 1, Job = "Okuma Operator", TimeStamp = DateTime.Now });
+            modelBuilder.Entity<OperatorJob>().HasData(new OperatorJob { OperatorJobId = 2, Job = "Diamond Turn Operator", TimeStamp = DateTime.Now });
+
+            var operators = new List<Operator>
+            {
+                new Operator {FirstName = "Kevin", LastName = "Dimmett"},
+                new Operator {FirstName = "Pedro", LastName = "Anglero"},
+                new Operator {FirstName = "Joe", LastName = "Stroud"},
+                new Operator {FirstName = "Ritchard", LastName = "Krouse"},
+                new Operator {FirstName = "Gregory", LastName = "Kuzmits"},
+                new Operator {FirstName = "Nick", LastName = "Powers"},
+                new Operator {FirstName = "Bryan", LastName = "Crowner"},
+                new Operator {FirstName = "Kenny", LastName = "Plencner"},
+                new Operator {FirstName = "Michael", LastName = "Plencner"},
+                new Operator {FirstName = "Paula", LastName = "Smith"},
+                new Operator {FirstName = "Gary", LastName = "Westerhouse"},
+                new Operator {FirstName = "Steve", LastName = "Ehardt"},
+                new Operator {FirstName = "Rollie", LastName = "Ball"},
+                new Operator {FirstName = "Curt", LastName = "Webb"},
+                new Operator {FirstName = "Kevin", LastName = "Rossaw"},
+                new Operator {FirstName = "Scott", LastName = "Higbee"},
+                new Operator {FirstName = "Joe", LastName = "Kil"},
+                new Operator {FirstName = "Tracey", LastName = "Smith"},
+                new Operator {FirstName = "Dale", LastName = "Beghtel"}
+            };
+
+            for (var i = 0; i < operators.Count; i++)
+            {
+                var op = operators[i];
+                op.OperatorId = i+1;
+                op.TimeStamp = DateTime.Now;
+                modelBuilder.Entity<Operator>().HasData(op);
+            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -38,6 +74,8 @@ namespace FmsbwebCoreApi.Context.Intranet
             {
                 throw new ArgumentNullException(nameof(modelBuilder));
             }
+
+            SeedData(modelBuilder);
 
             modelBuilder.HasAnnotation("Relational:DefaultSchema", "db_owner");
 
