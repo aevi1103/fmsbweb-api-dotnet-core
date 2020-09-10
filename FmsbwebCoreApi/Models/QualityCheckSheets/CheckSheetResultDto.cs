8446f38bdcf3ef594aaf8390424309ef0c332e1c
aevi1103 <aevirontos@gmail.com>
2020-09-10 06:26:13 -0400

    dto class

---
+++ FmsbwebCoreApi/Models/QualityCheckSheets/CheckSheetResultDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FmsbwebCoreApi.Models.QualityCheckSheets
+{
+    public class CheckSheetResultDto
+    {
+        public int Status { get; set; }
+        public string StatusText { get; set; }
+        public dynamic Result { get; set; }
+    }
+}
