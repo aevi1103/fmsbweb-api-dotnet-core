2eb58a49f5bcd4aa478f11ef8a6b3c7939012294
Aevi <aevirontos@gmail.com>
2020-12-07 07:52:19 -0500

    refactor controller

--- FmsbwebCoreApi/Controllers/OEE/OeeController.cs
+++ FmsbwebCoreApi/Controllers/OEE/OeeController.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FmsbwebCoreApi.Context.Iconics;
+using FmsbwebCoreApi.Hubs.Counter;
+using FmsbwebCoreApi.Hubs.Downtime;
+using FmsbwebCoreApi.Hubs.Scrap;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 
 namespace FmsbwebCoreApi.Controllers.OEE
@@ -14,10 +18,20 @@ namespace FmsbwebCoreApi.Controllers.OEE
     public class OeeController : ControllerBase
     {
         private readonly IconicsContext _context;
+        private readonly IHubContext<CounterHub> _counterHubContext;
+        private readonly IHubContext<ScrapHub> _scrapHubContext;
+        private readonly IHubContext<DowntimeHub> _downtimeHubContext;
 
-        public OeeController(IconicsContext context)
+        public OeeController(
+            IconicsContext context,
+            IHubContext<CounterHub> counterHubContext,
+            IHubContext<ScrapHub> scrapHubContext,
+            IHubContext<DowntimeHub> downtimeHubContext)
         {
             _context = context ?? throw new ArgumentNullException(nameof(context));
+            _counterHubContext = counterHubContext ?? throw new ArgumentNullException(nameof(counterHubContext));
+            _scrapHubContext = scrapHubContext ?? throw new ArgumentNullException(nameof(scrapHubContext));
+            _downtimeHubContext = downtimeHubContext ?? throw new ArgumentNullException(nameof(downtimeHubContext));
         }
 
         [HttpGet]
@@ -48,6 +62,7 @@ namespace FmsbwebCoreApi.Controllers.OEE
             {
                 var data = await _context.KepServerTagNameGroups
                     .Where(x => x.Department.ToLower() == department.ToLower().Trim())
+                    .OrderBy(x => x.GroupName)
                     .ToListAsync()
                     .ConfigureAwait(false);
 
@@ -59,5 +74,28 @@ namespace FmsbwebCoreApi.Controllers.OEE
                 throw new Exception(e.Message);
             }
         }
+
+        [HttpGet]
+        [Route("group/{guid}")] 
+        public async Task<ActionResult> GetGroup(Guid guid)
+        {
+            try
+            {
+                var data = await _context.KepServerTagNameGroups
+                    .FirstOrDefaultAsync(x => x.KepServerTagNameGroupId == guid)
+                    .ConfigureAwait(false);
+
+                //await AddToGroup(data.TagName, Guid.NewGuid().ToString()).ConfigureAwait(false);
+
+                return Ok(data);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw new Exception(e.Message);
+            }
+        }
+
+
     }
 }
