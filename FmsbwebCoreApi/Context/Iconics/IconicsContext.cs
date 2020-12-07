d946bdfb478c89431a7c1f71f08d95db2035c37e
Aevi <aevirontos@gmail.com>
2020-12-07 07:51:56 -0500

    modify see

--- FmsbwebCoreApi/Context/Iconics/IconicsContext.cs
+++ FmsbwebCoreApi/Context/Iconics/IconicsContext.cs
@@ -22,7 +22,6 @@ namespace FmsbwebCoreApi.Context.Iconics
         public virtual DbSet<KepserverTagNamesView> KepserverTagNamesView { get; set; }
         public virtual DbSet<KepServerTagName> KepServerTagNames { get; set; }
 
-        public virtual DbSet<KepServerTagNameMonitor> KepServerTagNameMonitors { get; set; }
         public virtual DbSet<KepServerTagNameGroup> KepServerTagNameGroups { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -39,58 +38,58 @@ namespace FmsbwebCoreApi.Context.Iconics
                 new
                 {
                     Line = "A2",
-                    Tag =  "A2_PACKOUT_APC",
-                    LineGuid = "01b888a3-00ca-4736-96ae-1636dc7ec914",
-                    TagGuid = "b37a8885-7889-407f-a44a-3cbcf3ee1a14"
+                    Tag =  "Assembly.A2_PACKOUT.A2_PACKOUT_APC",
+                    WorkCenter = "ASBY0002",
+                    Guid = "01b888a3-00ca-4736-96ae-1636dc7ec914"
                 },
                 new
                 {
                     Line = "A3",
-                    Tag =  "A3_APC",
-                    LineGuid = "bd9eae20-60c7-44cd-ae2e-96c5f015f82e",
-                    TagGuid = "9f1aabaf-fc1e-437e-8af3-2e3e17aa4192"
+                    Tag =  "Assembly.A3.A3_APC",
+                    WorkCenter = "ASBY0003",
+                    Guid = "bd9eae20-60c7-44cd-ae2e-96c5f015f82e"
                 },
                 new
                 {
                     Line = "A4",
-                    Tag =  "A4_M3_APC",
-                    LineGuid = "77e6d96c-f58d-46ea-a00d-5f962cfaf67b",
-                    TagGuid = "d81f4aca-b172-4e2d-b138-85fca35fdb48"
+                    Tag =  "Assembly.A4_M3.A4_M3_APC",
+                    WorkCenter = "ASBY0004",
+                    Guid = "77e6d96c-f58d-46ea-a00d-5f962cfaf67b"
                 },
                 new
                 {
                     Line = "A5",
-                    Tag =  "A5_APC",
-                    LineGuid = "c26e4341-8e94-48f8-9eb6-fd2c5061bae3",
-                    TagGuid = "978337b6-98c6-4f65-97da-bed63b415e4a"
+                    Tag =  "Assembly.A5.A5_APC",
+                    WorkCenter = "ASBY0005",
+                    Guid = "c26e4341-8e94-48f8-9eb6-fd2c5061bae3"
                 },
                 new
                 {
                     Line = "A6",
-                    Tag =  "A6_M2_APC",
-                    LineGuid = "f5ede641-fb76-40d5-924b-5d28ceea1ac9",
-                    TagGuid = "bb1862ce-201c-40b1-be25-c6f13f8ec6b1"
+                    Tag =  "Assembly.A6_M2.A6_M2_APC",
+                    WorkCenter = "ASBY0006",
+                    Guid = "f5ede641-fb76-40d5-924b-5d28ceea1ac9"
                 },
                 new
                 {
                     Line = "A7",
-                    Tag =  "A7_M3_APC",
-                    LineGuid = "1ace746f-f44f-47c4-bb9c-6c49fc550488",
-                    TagGuid = "e6273b83-641d-4669-8003-be5bdd928093"
+                    Tag =  "Assembly.A7_M3.A7_M3_APC",
+                    WorkCenter = "ASBY0007",
+                    Guid = "1ace746f-f44f-47c4-bb9c-6c49fc550488"
                 },
                 new
                 {
                     Line = "A8",
-                    Tag =  "A8_APC",
-                    LineGuid = "13667af3-8a37-456a-8d65-5e98627af1ad",
-                    TagGuid = "d43a0be0-32cc-4e28-9f74-88d10c04b6a8"
+                    Tag =  "Assembly.A8.A8_APC",
+                    WorkCenter = "ASBY0008",
+                    Guid = "13667af3-8a37-456a-8d65-5e98627af1ad"
                 },
                 new
                 {
                     Line = "A9",
-                    Tag =  "A9_M3_APC",
-                    LineGuid = "3ea0d0fe-9c1b-4171-baa1-aa5b6e6422e2",
-                    TagGuid = "61a26041-c823-427b-be03-9a10f37e344a"
+                    Tag =  "Assembly.A9_M3.A9_M3_APC",
+                    WorkCenter = "ASBY0009",
+                    Guid = "3ea0d0fe-9c1b-4171-baa1-aa5b6e6422e2"
                 },
             };
 
@@ -98,17 +97,11 @@ namespace FmsbwebCoreApi.Context.Iconics
             {
                 modelBuilder.Entity<KepServerTagNameGroup>().HasData(new KepServerTagNameGroup
                 {
-                    KepServerTagNameGroupId = new Guid(tag.LineGuid),
+                    KepServerTagNameGroupId = new Guid(tag.Guid),
                     GroupName = tag.Line,
-                    Department = "Assembly",
-                    TimeStamp = DateTime.Now
-                });
-
-                modelBuilder.Entity<KepServerTagNameMonitor>().HasData(new KepServerTagNameMonitor
-                {
-                    Id = new Guid(tag.TagGuid),
-                    KepServerTagNameGroupId = new Guid(tag.LineGuid),
                     TagName = tag.Tag,
+                    Department = "Assembly",
+                    WorkCenter = tag.WorkCenter,
                     TimeStamp = DateTime.Now
                 });
             }
